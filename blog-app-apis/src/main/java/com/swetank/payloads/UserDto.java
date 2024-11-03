70d96ab2c8cbcb1877372191276cf13d2e937e20
swetank601-ara <swetank601@gmail.com>
2024-11-03 11:03:37 +0530

    Third Commit

--- blog-app-apis/src/main/java/com/swetank/payloads/UserDto.java
+++ blog-app-apis/src/main/java/com/swetank/payloads/UserDto.java
@@ -2,6 +2,7 @@ package com.swetank.payloads;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -14,12 +15,14 @@ public class UserDto {
 	private int id;
 	
 	@NotEmpty
+	@Size(min=4,message="Username must be min of 4 characters")
     private String name;
 	
-	@Email
+	@Email(message="Email address is not valid !!")
 	private String email;
 	
 	@NotEmpty
+	@Size(min=3,max=10,message="Password must be min of 3 characters and max of 10 characters")
 	private String password;
 	
 	@NotEmpty
