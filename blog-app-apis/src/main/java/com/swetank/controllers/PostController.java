74edaf57137a751b3383e22b5e584bd029a5a403
swetank601-ara <swetank601@gmail.com>
2024-11-29 23:32:09 +0530

    Image Commit

--- blog-app-apis/src/main/java/com/swetank/controllers/PostController.java
+++ blog-app-apis/src/main/java/com/swetank/controllers/PostController.java
@@ -1,10 +1,16 @@
 package com.swetank.controllers;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,13 +20,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.swetank.configs.AppConstants;
 import com.swetank.payloads.ApiResponse;
+import com.swetank.payloads.ImageResponse;
 import com.swetank.payloads.PostDto;
 import com.swetank.payloads.PostResponse;
+import com.swetank.services.FileService;
 import com.swetank.services.PostService;
 
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 
 @RestController
@@ -29,6 +39,12 @@ public class PostController {
 	
 	@Autowired
 	private PostService postService;
+	
+	@Autowired
+	private FileService fileService;
+	
+	@Value("${project.image}")
+	private String path;
 
 	//create
 	@PostMapping("/user/{userId}/category/{categoryId}/posts")
@@ -101,5 +117,29 @@ public class PostController {
 		
 		List<PostDto> searchPosts = this.postService.searchPosts(keywords);
 		return new ResponseEntity<List<PostDto>>(searchPosts,HttpStatus.OK);
+	}
+	
+	//image upload
+	@PostMapping("/post/image/upload/{postId}")
+	public ResponseEntity<PostDto> uploadPostImage(
+			@RequestParam("image") MultipartFile image,
+			@PathVariable Integer postId
+			) throws IOException{
+		
+		PostDto postDto = this.postService.getPostById(postId);
+		String fileName = this.fileService.uploadImage(path, image);
+		postDto.setImageName(fileName);
+		PostDto updatePost = this.postService.updatePost(postDto, postId);
+		return new ResponseEntity<PostDto>(updatePost,HttpStatus.OK);
+	}
+	
+	//method to serve files
+	@GetMapping(value="/post/image/{imageName}",produces=MediaType.IMAGE_JPEG_VALUE)
+	public void downloadImage(
+			@PathVariable String imageName,
+			HttpServletResponse response) throws IOException {
+		InputStream resource = this.fileService.getResource(path, imageName);
+		response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+		StreamUtils.copy(resource, response.getOutputStream());
 	}
 }
---
+++ blog-app-apis/src/main/java/com/swetank/payloads/ImageResponse.java
@@ -0,0 +1,5 @@
+package com.swetank.payloads;
+
+public class ImageResponse {
+
+}
